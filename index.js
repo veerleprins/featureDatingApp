9e0caf04dee5e7ca3c8d896d8b65d4c669664f68
veerleprins <veerleprins@hotmail.com>
2020-02-27 13:25:10 +0100

    Copied old static HTML files to another directory with name old and deleted the public static HTML files

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
-// const camelCase = require('camelcase');
-// console.log(camelCase('FOO-BAR'));
-
 // Import express package:
 const express = require('express');
 const app = express();
-
-app.set('view engine', 'ejs');
-
-// Using PORT 3000:
 const PORT = 3000;
 
-// Using 'static' directory:
-app.use(express.static('public'));
-app.use(express.static('images'));
+//Setting ejs:
+app.set('view engine', 'ejs');
+//Telling ejs to get view directory:
+app.set('views', 'view');
 
-// Getting index.html file:
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
-// Getting about.html file:
-app.get('/about/', function (req, res) {
-    // res.sendFile(__dirname + '/public/about.html');
-    // console.log(req.query);
-    res.send(req.params)
-});
-// Getting contact.html file:
-app.get('/contact', function (req, res) {
-    res.sendFile(__dirname + '/public/contact.html');
-});
+// Using 'static' directory:
+app.use(express.static('static'));
+
+//Getting al the paths and calling the functions:
+app.get('/', home);
+app.get('/about', about);
+app.get('/profile', profile);
+app.get('/contact', contact);
+app.get('/*', error);
+
+function home(req, res) {
+    res.render('index');
+};
+
+function about(req, res) {
+    res.render('about');
+};
+
+function contact(req, res) {
+    res.render('contact');
+};
+
+function profile(req, res) {
+    res.render('profile');
+};
+
+function error(req, res) {
+    res.render('404');
+};
+
+// app.get('/', function (req, res) {
+//     res.render('index')
+// })
+
+// app.get('/about', function (req, res) {
+//     res.render('about')
+// })
+
+// app.get('/contact', function (req, res) {
+//     res.render('contact')
+// })
+
+// app.get('/profile', function (req, res) {
+//     res.render('profile');
+// })
+
+//Should always be on the bottom above app.listen:
+// app.get('/*', function (req, res) {
+//     res.render('404');
+// })
 
 // Getting dynamic page with de user's name:
-app.get('/profile/:name', function (req, res) {
-    // res.send(`Your name is: ${req.params.nameUser}`);
-    res.render('profile', {
-        person: req.params.name
-    });
-});
-
-// Getting 404.html file:
-app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '/public/404.html');
-});
+// app.get('/profile/:name', function (req, res) {
+//     // res.send(`Your name is: ${req.params.nameUser}`);
+//     res.render('profile', {
+//         person: req.params.name
+//     });
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
