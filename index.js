271e01eeccc5f6de3cb1ee2c2773d518cf320c4c
veerleprins <veerleprins@hotmail.com>
2020-03-02 17:52:41 +0100

    Updated to send something to the server but page keeps reloading

--- index.js
+++ index.js
@@ -2,21 +2,42 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
+
+let jsonParser = bodyParser.json()
+
+let urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+app.use(express.static('static'));
+
+let data = [{
+        id: "purge",
+        title: "The Purge",
+        description: "Description about the purge here..."
+    },
+    {
+        id: "silent-hill",
+        title: "Silent Hill",
+        description: "Description about Silent Hill here..."
+    },
+    {
+        id: "conjuring",
+        title: "The Conjuring",
+        description: "Description about The Conjuring here..."
+    }
+];
 
 //Setting ejs:
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 //Telling ejs to get view directory:
-// app.set('views', 'view-ejs');
-
-app.set('view engine', 'pug');
-app.set('views', 'view-pug');
-
-// Using 'static' directory:
-app.use(express.static('static'));
+app.set('views', 'view-ejs');
 
 //Getting al the paths and calling the functions:
 app.get('/', home);
 app.get('/about', about);
+app.post('/contact', urlencodedParser, postFunction);
 app.get('/profile', profile);
 app.get('/contact', contact);
 app.get('/*', error);
@@ -26,12 +47,19 @@ function home(req, res) {
 };
 
 function about(req, res) {
-    res.render('about');
+    res.render('about', {
+        data: data
+    });
 };
 
 function contact(req, res) {
     res.render('contact');
 };
+
+function postFunction(req, res) {
+    console.log(req.body);
+    return
+}
 
 function profile(req, res) {
     res.render('profile');
