ff7ccf405f2b848a480cddf46aff99260fe7f247
veerleprins <veerleprins@hotmail.com>
2020-03-08 23:50:11 +0100

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    'rules': {
+        'indent': [
+            'warn',
+            1
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            0
+        ]
+    }
+};
