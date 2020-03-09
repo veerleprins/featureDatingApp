c8e9bc6a0c9dec417a144d52c9fa7b11b2c7c259
veerleprins <veerleprins@hotmail.com>
2020-03-09 22:50:40 +0100

    Copied old files to folder within .ignore and deleted them in the view-ejs folder

--- old_index.js
+++ old_index.js
@@ -16,32 +16,32 @@ app.use(express.static('images'));
 
 // Getting index.html file:
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+ res.sendFile(__dirname + '/public/index.html');
 });
 // Getting about.html file:
 app.get('/about/', function (req, res) {
-    // res.sendFile(__dirname + '/public/about.html');
-    // console.log(req.query);
-    res.send(req.params)
+ // res.sendFile(__dirname + '/public/about.html');
+ // console.log(req.query);
+ res.send(req.params)
 });
 // Getting contact.html file:
 app.get('/contact', function (req, res) {
-    res.sendFile(__dirname + '/public/contact.html');
+ res.sendFile(__dirname + '/public/contact.html');
 });
 
 // Getting dynamic page with de user's name:
 app.get('/profile/:name', function (req, res) {
-    // res.send(`Your name is: ${req.params.nameUser}`);
-    res.render('profile', {
-        person: req.params.name
-    });
+ // res.send(`Your name is: ${req.params.nameUser}`);
+ res.render('profile', {
+  person: req.params.name
+ });
 });
 
 // Getting 404.html file:
 app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '/public/404.html');
+ res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+ console.log(`Server is listening on port ${PORT}`);
 });
