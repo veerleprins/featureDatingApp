d1325075d7224255830af6cc9035ad3eb3ff6c65
veerleprins <veerleprins@hotmail.com>
2020-03-10 23:17:02 +0100

    Started with localstorage

---
+++ static/scripts/clientScript.js
@@ -0,0 +1,15 @@
+// localStorage.setItem("key", "value");
+
+//Trying to make local storage work:
+let minimumAge = document.getElementById("minAge");
+let maximumAge = document.getElementById("maxAge");
+let distance = document.getElementById("distance");
+
+let button = document.getElementById("Submit");
+
+// button.onclick = function () {
+//   let key = minimumAge.value;
+//   let value = minimumAge.value;
+// } 
+// localStorage.setItem(key, value);
+// location.reload()
