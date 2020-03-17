6bf05739aaade09a9f902274d5a940ddb6afd155
veerleprins <veerleprins@hotmail.com>
2020-03-17 16:37:45 +0100

    Update database with gender preferences

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Import packages:
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongo = require('mongodb').MongoClient;
+const mongo = require('mongodb');
 const slug = require('slug');
 const dotenv = require('dotenv').config();
 const app = express();
 const PORT = 3000;
 const urlencodedParser = bodyParser.urlencoded({extended: true});
 
-let usersCollection = null;
 let idThisUser = 1;
 let allUsers;
 let db = null;
-let uri = process.env.DB_HOST + ':' + process.env.DB_PORT;
+let url = process.env.DB_HOST + ':' + process.env.DB_PORT;
 
-mongo.connect(uri, { useUnifiedTopology: true }, function (err, client){
-  if (err) throw err;
+mongo.MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client){
+  if (err) {
+    throw err
+  }
   db = client.db(process.env.DB_NAME);
-  usersCollection = db.collection('users');
   console.log('Database is connected...');
 });
 
@@ -101,21 +101,32 @@ app.get('/filters', filters);
 app.get('/*', error);
 app.post('/', postFilters);
 
+//Listen to the port:
+app.listen(PORT, connected);
+
 function filters(req, res){
-  // let data = slug(req.body);
-  res.render('/', {
-    preferences: req.body
-  });
+    db.collection('datingUsers').findOne({_id: ('5e70df1afab0ce3cb81154f8')}, test);
+
+    function test(err, users) {
+      if (err) {
+        next(err)
+      } else {
+        // let data = slug(req.body);
+        res.render('/', { preferences: req.body }) ;
+      }
+    }
 }
 
 function home(req, res) {
-  usersCollection.find().toArray(done)
+  db.collection('datingUsers').find().toArray(done)
+
   function done(err, users) {
     //Get the index of the loggedIn user: 
     let index = users.findIndex(p => p.id === idThisUser);
     //Remove own user from allUsers: 
     allUsers = users;
     allUsers.splice(index, 1);
+
       if (err) {
           next(err);
       } else {
@@ -125,29 +136,82 @@ function home(req, res) {
 };
 
 function postFilters (req, res){
-  let preferences =
-  usersCollection.updateOne({"id": idThisUser}, {$set: pref}, function(err, result){
+  db.collection('datingUsers').findOne({id: 1}, test);
+  // db.collection('datingUsers').findOne({_id: "5e70df1afab0ce3cb81154f8"}, test);
+  // db.collection('datingUsers').insertOne({ _id: ('5e70df1afab0ce3cb81154f8')}, { $set: { prefGender: req.body.gender }})
 
-  })
-
-
-  console.log(db.getUsers({filter: {id : 1}}));
-  usersCollection.find().toArray(done)
-  function done(err, users) {
-    //Get the OBJECT of the loggedIn user: 
-    let loggedIn = users.find(obj => obj.id === idThisUser);
-    console.log(loggedIn);
-    let gender = req.body.gender;
-    let movie = req.body.movies;
-    let newpref = [gender, movie];
+  function test(err, user) {
     if (err) {
-      next(err);
+      next(err)
     } else {
-      loggedIn.update({pref : []}, {$set : {pref: newpref}})
-      console.log(loggedIn);
-      res.render('index.ejs', { users: usersData });
-      }
-  }};
+      db.collection('datingUsers').updateOne({id: 1}, { $set: { prefGender: req.body.gender }})
+      console.log(user);
+
+      // if ( == 'anything') {
+
+      //   // if PrefGender === gender || prefGender === 'anything' => show  db.collection('datingUsers').find().toArray(done)
+      //   // functie done
+
+
+      // }
+      res.render('index.ejs', { users: user})
+    }
+  }
+        //  if (err) {
+        //     next(err);
+        // } else {
+        //   res.render('index.ejs', {users: usersData});
+
+        // }
+  // preference = req.body.gender;
+  // db.collection('users').updateOne({ _id: ('5e6f9eadfab0ce3cb81154f0')}, { $set: { prefGender: req.body.gender }})
+
+
+  // function test (err, user) {
+  //   if (err) {
+  //     next(err)
+  //   }else {
+  //     db.collection('users').updateOne({prefGender: ''}, {$set: preference});
+  //   }
+  // } 
+};
+
+
+// let index = users.findIndex(p => p.id === yourSelf);
+// completeCollection = users;
+// completeCollection.splice(index, 1);
+// // console.log(allUsers)
+
+// let x = (completeCollection.length - 1);
+
+
+// if (req.body.like) {
+//     usersCollection.updateOne({ _id: (completeCollection[x]._id) }, { $set: { match: true } })
+
+//     console.log(`you have a like with ${completeCollection[x].name}, and the ID is ${completeCollection[x]._id}`)
+
+
+
+
+  // usersCollection.find().toArray(done)
+  // function done(err, users) {
+  //   //Get the OBJECT of the loggedIn user: 
+  //   if (err) {
+  //     next(err)
+  //   } else {
+  //     loggedIn = users.find(obj => obj.id === idThisUser);
+  //     usersCollection.updateOne({prefGender :})
+  //   }
+    // res.render('index.ejs', { users: usersData });
+    // // console.log('_________________________');
+    // // console.log(loggedIn["pref"]);
+    // // console.log('_________________________');
+    // if (err) {
+    //   next(err);
+    // } else {
+    //   // hier naar kijken: loggedIn["pref"].update()
+    //   }
+
 
   
   // usersCollection.find().toArray(done);
@@ -221,7 +285,7 @@ function error(req, res) {
  res.render('404');
 };
 
-//Server listening on given port:
-app.listen(PORT, () => {
- console.log(`Server is listening on port ${PORT}`);
-})
+function connected() {
+  //Server listening on given port:
+  console.log(`Server is listening on port ${PORT}`);
+}
