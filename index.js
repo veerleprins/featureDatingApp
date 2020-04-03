84dfaed2e978ed19f0a02ebe4a4a94ebcffd44a0
veerleprins <veerleprins@hotmail.com>
2020-04-03 12:59:40 +0200

    Changed 404 page with text and button, index with error when no users are shown and index.js with clean code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Import packages:
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const mongo = require('mongodb').MongoClient;
-const slug = require('slug');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const mongo = require("mongodb").MongoClient;
+const slug = require("slug");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 //Setting the global variables: 
 const DB_PORT = process.env.DB_PORT;
@@ -13,7 +13,7 @@ const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 let idThisUser = 1;
 let db = null;
-let url = DB_HOST + ':' + DB_PORT;
+let url = DB_HOST + ":" + DB_PORT;
 
 //Connecting to the database:
 mongo.connect(url, { useUnifiedTopology: true }, function (err, client){
@@ -21,13 +21,13 @@ mongo.connect(url, { useUnifiedTopology: true }, function (err, client){
     throw err
   }
   db = client.db(DB_NAME);
-  console.log('Database is connected...');
+  console.log("Database is connected...");
 });
 
 //Setting and using the middleware:
-app.set('view engine', 'ejs');
-app.set('views', 'view-ejs');
-app.use(express.static('static'));
+app.set("view engine", "ejs");
+app.set("views", "view-ejs");
+app.use(express.static("static"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -36,52 +36,85 @@ app.use(session({
 }));
 
 //Getting all the paths and calling the functions:
-app.get('/', home);
-app.get('/filters', filters);
-app.post('/', postPreferences);
-app.get('/*', error);
+app.get("/", home);
+app.get("/filters", filters);
+app.post("/", postPreferences);
+app.get("/*", error);
 
 //Listen to the server:
 app.listen(DB_PORT, connected);
 
-function getOwn(user) {
-  return user.id === idThisUser;
-}
-
-function filterPreferences(users, prefgender, prefmovie) {
-  return users.filter(function (user) {
-
-    if ((user.gender === prefgender || prefgender === "Everyone") && prefmovie === "") {
-      return true
-    } else if ((user.gender === prefgender || prefgender === "Everyone") && prefmovie !== "") {
-      return user.movies.find(function (movie){
-        return movie === prefmovie;
-      })
-    }
-  });
-}
 
 async function home(req, res, next) {
-  //Displays the index page with all the users from the database
-  //except the logged in user:
+  //Shows the index page with the filtered users and stores this
+  //user's preferences as a session.
   try {
-    const datingUsers = await db.collection('datingUsers').find().toArray();
-    const usersWithout = await db.collection('datingUsers').find({id: {$ne: idThisUser}}).toArray();
+    const datingUsers = await db.collection("datingUsers").find().toArray();
+    let usersWithoutOwn = datingUsers.filter(removeOwn);
     let thisUser = datingUsers.filter(getOwn);
     req.session.gender = thisUser[0].prefGender;
     req.session.movie = thisUser[0].prefMovies;
 
-    let filtered = filterPreferences(usersWithout, thisUser[0].prefGender, thisUser[0].prefMovies);
-    res.render('index.ejs', {users: filtered});
+    let filtered = filterPreferences(usersWithoutOwn, thisUser[0].prefGender, thisUser[0].prefMovies);
+    res.render("index.ejs", {users: filtered});
   } catch (err) {
     next(err);
   }
-};
+}
+
+function removeOwn(user) {
+  //Returns all users who do not have the same ID as this user's ID:
+  return user.id !== idThisUser;
+}
+
+function getOwn(user) {
+  //Returns the user whose ID is the same as this user's ID:
+  return user.id === idThisUser;
+}
+
+function filterPreferences(users, prefgender, prefmovie) {
+  //Filters the users by gender and movie preferences and returns
+  //a boolean if the conditions are correct:
+  return users.filter(function (user) {
+
+    if ((user.gender === prefgender || prefgender === "Everyone") && prefmovie === "") {
+      return true;
+    } else if ((user.gender === prefgender || prefgender === "Everyone") && prefmovie !== "") {
+      return user.movies.find(function (movie){
+        return movie === prefmovie;
+      });
+    }
+  });
+}
+
+function filters(req, res) {
+  //Displays the filter page:
+  res.render("filters", {gender : req.session.gender, movie: req.session.movie});
+}
+
+async function postPreferences(req, res, next) {
+  //Retrieves the entered preferences and sends them to the 
+  //updatePreferences function. After this the index page is 
+  //redirected again:
+  try {
+    if (req.body.remove === "Remove preferences") {
+      req.session.destroy();
+      await updatePreferences("Everyone", "");
+    } else {
+      let prefgender = slug(req.body.gender);
+      let prefmovie = slug(req.body.movies);
+      await updatePreferences(prefgender, prefmovie);
+    }
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+}
 
 async function updatePreferences (gender, movie) {
-  //Updates the database with the information form the form:
+  //Updates the database with the new preferences from the form:
   try {
-    await db.collection('datingUsers').updateOne(
+    await db.collection("datingUsers").updateOne(
       {id: idThisUser},
       {$set: { prefGender: gender, prefMovies: movie}}
     );
@@ -90,58 +123,9 @@ async function updatePreferences (gender, movie) {
   }
 }
 
-
-async function postPreferences(req, res, next) {
-  try {
-    let prefgender = slug(req.body.gender);
-    let prefmovie = slug(req.body.movies);
-    await db.collection('datingUsers').updateOne(
-      {id: idThisUser},
-      {$set: { prefGender: prefgender, prefMovies: prefmovie}}
-    );
-    res.redirect('/');
-  } catch (err) {
-    next(err);
-  }
-}
-
-async function post (req, res, next) {
-  //Searches for users that match the logged-in user's preferences
-  // and show the index page with these filtered users:
-  req.session.gender = req.body.gender;
-  req.session.movie = req.body.movies;
-  try {
-    let prefgender = slug(req.body.gender);
-    let prefmovie = slug(req.body.movies);
-
-    updatePreferences(prefgender, prefmovie);
-    let allUsers = await db.collection('datingUsers').find({id: {$ne: idThisUser}}).toArray();
-    let loggedIn = await db.collection('datingUsers').find({id: idThisUser}).toArray();
-    let showUsers = await db.collection('datingUsers').find({$and : [{gender : prefgender}, {movies : prefmovie}]}).toArray();
-    let noMovies = await db.collection('datingUsers').find({$and : [{id: {$ne: idThisUser}}, {gender : prefgender}]}).toArray();
-    let everyone = await db.collection('datingUsers').find({$and : [{$or : [{gender : prefgender}, {movies : prefmovie}]}, {id: {$ne: idThisUser}}]}).toArray();
-    if (loggedIn[0].prefMovies === "" && loggedIn[0].prefGender === "Everyone") {
-      res.render('index.ejs', {users : allUsers});
-    } else if (loggedIn[0].prefMovies === "") {
-      res.render('index.ejs', {users: noMovies});
-    } else if (loggedIn[0].prefGender === "Everyone") {
-      res.render('index.ejs', {users: everyone});
-    } else {
-      res.render('index.ejs', {users: showUsers});
-    }
-  } catch (err) {
-    next(err);
-  }
-}
-
-function filters(req, res) {
-  //Displays the filter page:
-  res.render('filters', {gender : req.session.gender, movie: req.session.movie});
-};
-
 function error(req, res) {
   //Displays the error page:
- res.render('404');
+ res.render("404");
 };
 
 function connected() {
