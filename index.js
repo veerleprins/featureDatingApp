8cb3644e27663da450b6129328c85285c9f16c13
veerleprins <veerleprins@hotmail.com>
2020-04-05 00:32:53 +0200

    Modified index.js for heroku online

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 //Setting the global variables: 
 // const DB_PORT = process.env.DB_PORT;
-const DB_PORT = process.env.PORT || 3000 ;
+const DB_PORT = process.env.PORT || process.env.DB_PORT;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 let idThisUser = 1;
